cf4512a5ba280485a03f0090021478ca414c0330
Timothy-py <adeyeyetimothy33@gmail.com>
2022-08-11 23:29:19 +0100

    implemented function to setup the test environment

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -2,3 +2,23 @@ import unittest
 from .. import create_app
 from config import config_dict
 from ..models import User
+
+
+class AuthTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """
+        setup the test environment
+        """
+
+        # load an instance of the flask app with tesing configurations
+        self.app = create_app(config=config_dict['testing'])
+
+        # create app context
+        self.appctx = self.app.app_context()
+
+        # bind the app context to the current context
+        self.appctx.push()
+
+        # create a test client for this app
+        self.client = self.app.test_client()
